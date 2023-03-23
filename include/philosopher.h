f609b46f075feeec532d1f9489607c69b5eafb36
Mathieu Roy <mathieuroy1983@gmail.com>
2023-03-23 14:25:44 -0400

    commit

--- include/philosopher.h
+++ include/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: mroy <mroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:21:35 by mroy              #+#    #+#             */
-/*   Updated: 2023/03/23 11:54:46 by mroy             ###   ########.fr       */
+/*   Updated: 2023/03/23 13:56:04 by mroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ typedef struct s_philo
 {
 	int32_t			name;
 	t_param			*param;
+	pthread_mutex_t	*forks_auth;
 	pthread_mutex_t	*left_fork_auth;
 	pthread_mutex_t	*right_fork_auth;
 	pthread_mutex_t	*left_fork;
--- src/philosophers.c
+++ src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mroy <mroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 08:44:52 by math              #+#    #+#             */
-/*   Updated: 2023/03/23 11:52:38 by mroy             ###   ########.fr       */
+/*   Updated: 2023/03/23 14:12:03 by mroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,21 +60,25 @@ static inline void	thinking(t_philo *ph)
 void	*philo_work(void *philo)
 {
 	t_philo			*ph;
-	pthread_mutex_t	*both_forks;
 	uint64_t		prev_meal;
 
 	ph = (t_philo *) philo;
 	while (true)
 	{
 		pthread_mutex_lock(ph->forks_auth);
-		if (!ph->is_authorized)
+		if (ph->is_authorized)
 		{
-			pthread_mutex_unlock(ph->forks_auth);
+			pthread_mutex_lock(ph->left_fork_auth);
+			pthread_mutex_lock(ph->right_fork_auth);
+			prev_meal = ph->last_meal;
+			take_forks(ph);
+			ph->is_authorized = false;
+		}
+		else 
+		{
+			pthread_mutex_lock(ph->right_fork_auth);
 			continue ;
 		}
-		prev_meal = ph->last_meal;
-		take_forks(ph);
-		ph->is_authorized = false;
 		pthread_mutex_unlock(ph->forks_auth);
 		eating(ph, prev_meal);
 		sleeping(ph);
