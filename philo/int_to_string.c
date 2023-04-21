47f7ceb9b12db246c7c1c6f68260d563fd5e8eed
Mathieu Roy <mathieuroy1983@gmail.com>
2023-04-21 08:29:09 -0400

    commit

--- philo/int_to_string.c
+++ philo/int_to_string.c
@@ -6,13 +6,13 @@
 /*   By: mroy <mroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 08:44:52 by math              #+#    #+#             */
-/*   Updated: 2023/04/19 16:29:32 by mroy             ###   ########.fr       */
+/*   Updated: 2023/04/21 08:28:30 by mroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-uint32_t	digits10(uint64_t v)
+static uint32_t	digits10(uint64_t v)
 {
 	if (v < 10)
 		return (1);
@@ -41,6 +41,23 @@ uint32_t	digits10(uint64_t v)
 	return (12 + digits10(v / 1000000000000));
 }
 
+inline static size_t	convert_loop(size_t next, uint32_t i,
+	uint32_t value, char *dst)
+{
+	static const char	digits[201]
+		= "0001020304050607080910111213141516171819"
+		"2021222324252627282930313233343536373839"
+		"4041424344454647484950515253545556575859"
+		"6061626364656667686970717273747576777879"
+		"8081828384858687888990919293949596979899";
+
+	i = (value % 100) * 2;
+	value /= 100;
+	dst[next] = digits[i + 1];
+	dst[next - 1] = digits[i];
+	next -= 2;
+}
+
 size_t	uint32_to_str(uint32_t value, char *dst)
 {
 	static const char	digits[201]
@@ -56,13 +73,7 @@ size_t	uint32_to_str(uint32_t value, char *dst)
 	i = 0;
 	next = length - 1;
 	while (value >= 100)
-	{
-		i = (value % 100) * 2;
-		value /= 100;
-		dst[next] = digits[i + 1];
-		dst[next - 1] = digits[i];
-		next -= 2;
-	}
+		next = convert_loop(next, i, value, dst);
 	if (value < 10)
 		dst[next] = '0' + (value);
 	else
